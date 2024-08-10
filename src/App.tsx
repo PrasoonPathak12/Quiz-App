d11f1978dcce8f99b1dd1695dbbf2dfe41717aff
unknown <pathakprasoon37@gmail.com>
2024-08-10 18:46:51 +0530

    Made changes to the components...

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './App.css'
-
-interface quizQuestion{
-  difficulty:string,
-  question:string
-}
-
+import QuestionCard from './components/QuestionCard';
 function App() {
 
-  const[data,setData] = useState<quizQuestion[]>([]);
-
-  useEffect(()=>{
-    fetch('https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple')
-    .then((response)=>response.json())
-    .then((data)=>{
-      setData(data.results)
-  })
-  },[])
+  
 
   return (
-    <div className="App">
-     
-      <p className='ps'>Quiz-App 🤞</p>
-      
-       { <div className='quiz-box moo'>
-         
-       </div> }
+  
+    <div className='page'>
+        <div className='content'>
+
+          <QuestionCard/>   
+        </div>
 
     </div>
+      
+      
+    
   );
 }
 
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useEffect,useState } from 'react';
+
+function QuestionCard() {
+
+  return (
+    <div className='questionBox'>
+    A div that contains questions...
+    </div>
+  )
+}
+
+export default QuestionCard;
+
+
